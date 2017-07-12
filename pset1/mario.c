71185b349bc1ecb6f6affe76e00040643bf7a9c9
Stanislav Pavlovič <stanislav.pavlovic@gmail.com>
2017-07-12 07:00:55 +0000

    style changes to mario.c

--- pset1/mario.c
+++ pset1/mario.c
@@ -5,15 +5,18 @@
 // height = height of pyramid = width of half pyramid without middle space
 // width = width of half of the pyramid
 
-void print_floor(width,height) {
+void print_floor(width,height) 
+{
     
-    //prints spaces needed to fill first floor
-    for (int i = 0; i<height-width; i++) {
+    //prints spaces needed to fill first half
+    for (int i = 0; i<height-width; i++) 
+    {
         printf(" ");
     }
     
     //prints half of pyramid
-    for (int i = 0; i<width; i++) {
+    for (int i = 0; i<width; i++) 
+    {
         printf("#");
     }
     
@@ -21,24 +24,28 @@ void print_floor(width,height) {
     printf("  ");
     
     //prints second half of pyramid
-    for (int i = 0; i<width; i++) {
+    for (int i = 0; i<width; i++) 
+    {
         printf("#");
     }
     printf("\n");
 }
 
-void print_pyramid(height) {
+void print_pyramid(height) 
+{
     int width = 1; //top level width in "#" per half
     
     //print pyramid's floors
-    for (int i = 0; i<height; i++)   {
+    for (int i = 0; i<height; i++)   
+    {
         print_floor(width, height);
         width++; //increment for each level  (+ "#" for each half)
     }
 }
 
 //asks for height, if not in interval, asks again
-int get_height(void) {
+int get_height(void) 
+{
     int height = 0;
     do
     {
@@ -49,7 +56,8 @@ int get_height(void) {
     return height;
 }
 
-int main(void) {
+int main(void) 
+{
     int height = get_height(); //get height
     print_pyramid(height); //prints pyramid
 }
